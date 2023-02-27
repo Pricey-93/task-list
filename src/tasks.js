6caeaf675ecdeb0effa3ae728004922e4c8de38b
Dan <danieljamieprice@gmail.com>
2023-02-28 02:41:17 +0900

    Simplify code

--- src/tasks.js
+++ src/tasks.js
@@ -1,58 +1,31 @@
 import cross from './assets/images/icon-cross.svg';
 
-let labelCounter = 1;
-
-function incrementLabelCounter() {
-  labelCounter += 1;
+export function removeTask(target) {
+  target.closest('.task').remove();
 }
-// function decrementCounter() {
-//   counter -= 1;
-// }
 
-export const tasksContainer = document.createElement('div');
-tasksContainer.id = 'tasks-container';
-
-export function createTask(todo) {
-  const container = document.createElement('div');
+export function createTask(task) {
+  const container = document.createElement('li');
   container.classList.add('task');
 
   const checkBox = document.createElement('input');
-  checkBox.id = `checkbox-${labelCounter}`;
+  checkBox.id = 'checkbox-2';
   checkBox.classList.add('check-box');
   checkBox.type = 'checkbox';
 
   const description = document.createElement('label');
-  description.htmlFor = checkBox.id;
   description.classList.add('checkbox-label');
-  description.textContent = todo;
+  description.htmlFor = 'checkbox-2';
+  description.textContent = task;
 
   const deleteIcon = document.createElement('img');
-  deleteIcon.src = cross;
   deleteIcon.classList.add('delete-icon');
+  deleteIcon.src = cross;
+  deleteIcon.alt = 'A cross image that will delete your task when clicked';
+  deleteIcon.addEventListener('click', (e) => {
+    removeTask(e.target);
+  });
 
   container.append(checkBox, description, deleteIcon);
-  tasksContainer.appendChild(container);
-
-  incrementLabelCounter();
   return container;
-}
-
-// export function removeTask() {
-// fill this out later
-// }
-
-export function createStatusInfo() {
-  const statusInfoContainer = document.createElement('div');
-  statusInfoContainer.id = 'tasks-status-container';
-
-  const remainingTasks = document.createElement('p');
-  remainingTasks.id = 'remaining-tasks';
-  remainingTasks.textContent = 'items left';
-
-  const clear = document.createElement('p');
-  clear.id = 'clear-action';
-  clear.textContent = 'Clear Completed';
-
-  statusInfoContainer.append(remainingTasks, clear);
-  return statusInfoContainer;
 }
