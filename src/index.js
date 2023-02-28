8c0279e5f887e512d056f3fa252785227e12cdfc
Dan <danieljamieprice@gmail.com>
2023-02-28 22:20:57 +0900

    Implement local storage loading for  tasks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import * as tasks from './tasks';
+import createTask from './tasks';
 
 const taskList = document.getElementById('task-list');
-taskList.append(tasks.createTask('test'));
+
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  tasks.forEach((task) => {
+    taskList.appendChild(createTask(task));
+  });
+}
 
 const inputField = document.getElementById('input-field');
 const form = document.getElementById('input-container');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  taskList.appendChild(tasks.createTask(inputField.value));
+  taskList.appendChild(createTask(inputField.value));
   inputField.value = '';
 });
+// localStorage.clear();
+if (localStorage.length > 0) {
+  window.onload = loadTasks();
+}
